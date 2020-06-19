8502cecce7caff8bc8fca4aa7af26809e3e66d1a
Serioja Karelin <karelinse@gmail.com>
2020-06-19 15:54:17 +0300

    bugfix: исправления ошибок после 2ого ревью

--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -1,6 +1,6 @@
 const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
-const popupCloseIcon = document.querySelector('.popup__close-button');
+const popupCloseButton = document.querySelector('.popup__close-button');
 const popupInputElName = document.querySelector('.popup__input_el_name');
 const popupInputElJob = document.querySelector('.popup__input_el_job');
 const profileName = document.querySelector('.profile__name');
@@ -8,9 +8,13 @@ const profileJob = document.querySelector('.profile__job');
 const popupForms = document.querySelector('.popup__forms');
 
 function popupToggle() {
-    popup.classList.toggle('popup_opened');
-    popupInputElName.value = profileName.textContent;
-    popupInputElJob.value = profileJob.textContent;
+    if (popup.classList.contains('popup_opened')) {    
+        popup.classList.toggle('popup_opened');
+    } else {
+        popup.classList.toggle('popup_opened')
+        popupInputElName.value = profileName.textContent;
+        popupInputElJob.value = profileJob.textContent;
+    }
 }
 
 function formSubmitHandler(evt) {
@@ -19,9 +23,9 @@ function formSubmitHandler(evt) {
     profileName.textContent = popupInputElName.value;
     profileJob.textContent = popupInputElJob.value;
 
-    popup.classList.remove('popup_opened');
+    popupToggle();
 }
 
 profileEditButton.addEventListener('click', popupToggle);
-popupCloseIcon.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
 popupForms.addEventListener('submit', formSubmitHandler);
