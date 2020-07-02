1c4666a8feea2cede3c7a74a8834343a88419630
Serioja Karelin <karelinse@gmail.com>
2020-07-02 21:37:39 +0300

    bugfix: изменения js

--- scripts/index.js
+++ scripts/index.js
@@ -12,29 +12,32 @@ const popupAddCloseButton = document.querySelector('.popup__close-button_type_ad
 const popupFormsAdd = document.querySelector('.popup__forms_type_add');
 const popupInputElPlaceName = document.querySelector('.popup__input_el_place-name');
 const popupInputElPlaceUrl = document.querySelector('.popup__input_el_place-url');
-const gallery = document.querySelector('.gallery');
+const cardsContainer = document.querySelector('.gallery');
 const cardTemplate = document.querySelector('.card-template');
+const popupView = document.querySelector('.popup_type_view');
+const popupViewCloseButton = document.querySelector('.popup__close-button_type_view');
 
-function popupEditToggle() {
+function popupToggle(popup) {
+    popup.classList.toggle('popup_opened');
+    document.activeElement.blur();
+}
+
+function handleEditPopup() {
     if (!popupEdit.classList.contains('popup_opened')) {    
         popupInputElName.value = profileName.textContent;
         popupInputElJob.value = profileJob.textContent;
     }
-
-    document.activeElement.blur();
-
-    popupEdit.classList.toggle('popup_opened');
+    
+    popupToggle(popupEdit);
 }
 
-function popupAddToggle() {
+function handleAddPopup() {
     if (!popupAdd.classList.contains('popup_opened')) {    
         popupInputElPlaceUrl.value  = '';
         popupInputElPlaceName.value = '';
     }
 
-    document.activeElement.blur();
-
-    popupAdd.classList.toggle('popup_opened');
+    popupToggle(popupAdd);
 }
 
 function formEditSubmitHandler(evt) {
@@ -43,83 +46,56 @@ function formEditSubmitHandler(evt) {
     profileName.textContent = popupInputElName.value;
     profileJob.textContent = popupInputElJob.value;
 
-    popupEditToggle();
+    handleEditPopup();
 }
 
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+function handleViewPopup(item) {
+
+    document.querySelector('.popup__photo').src = item.link; 
+    document.querySelector('.popup__photo-caption').textContent = item.name;
+
+    popupToggle(popupView);
+}
+
+popupViewCloseButton.addEventListener('click', function() {
+    popupToggle(popupView);
+});
+
+function handleLikeButton(evt) {
+    const likeButton = evt.target.closest('.gallery__like-button');
+    likeButton.classList.toggle('gallery__like-button_active');
+}
+
+function handleTrashButton(evt) {
+    const card = evt.target.closest('.gallery__card');
+    card.remove();
+}
 
 function addCard(item) {
     const card = cardTemplate.content.cloneNode(true);
     card.querySelector('.gallery__photo').src = item.link; 
     card.querySelector('.gallery__photo-title').textContent = item.name; 
 
-    gallery.prepend(card);
+    const likeButton = card.querySelector('.gallery__like-button');
+    likeButton.addEventListener('click', handleLikeButton);
 
-    const likeButton = gallery.querySelector('.gallery__like-button');
+    const trashButton = card.querySelector('.gallery__trash-button');
+    trashButton.addEventListener('click', handleTrashButton); 
 
-    function like(evt) {
-        const likeButton = evt.target.closest('.gallery__like-button');
-        likeButton.classList.toggle('gallery__like-button_active');
-    }
+    const galleryPhoto = card.querySelector('.gallery__photo');
+    galleryPhoto.addEventListener('click', function() {
+        handleViewPopup(item);
+    });
 
-    likeButton.addEventListener('click', like);
-
-    const trashButton = document.querySelector('.gallery__trash-button');
-
-    function trash(evt) {
-        const card = evt.target.closest('.gallery__card');
-        card.remove();
-    }
-
-    trashButton.addEventListener('click', trash); 
-
-    const popupView = document.querySelector('.popup_type_view');
-    const galleryPhoto = document.querySelector('.gallery__photo');
-    const popupViewCloseButton = document.querySelector('.popup__close-button_type_view');
-
-    function popupViewOpen(evt) {
-        popupView.classList.toggle('popup_opened');
-
-        document.querySelector('.popup__photo').src = item.link; 
-        document.querySelector('.popup__photo-caption').textContent = item.name;
-
-    }
-
-    galleryPhoto.addEventListener('click', popupViewOpen);
-
-    function popupViewClose(evt) {
-        popupView.classList.remove('popup_opened');
-    }
-
-    popupViewCloseButton.addEventListener('click', popupViewClose);
+    return card
 }
 
-initialCards.forEach(addCard);
+function renderCard(item) {
+    const card = addCard(item);
+    cardsContainer.prepend(card);
+}
+
+initialCards.forEach(renderCard);
 
 function formAddSubmitHandler(evt) {
     evt.preventDefault();
@@ -129,14 +105,14 @@ function formAddSubmitHandler(evt) {
         link: popupInputElPlaceUrl.value
     }
 
-    addCard(item);
+    renderCard(item);
 
-    popupAddToggle();
+    handleAddPopup();
 }
 
-profileEditButton.addEventListener('click', popupEditToggle);
-popupEditCloseButton.addEventListener('click', popupEditToggle);
+profileEditButton.addEventListener('click', handleEditPopup);
+popupEditCloseButton.addEventListener('click', handleEditPopup);
 popupFormsEdit.addEventListener('submit', formEditSubmitHandler);
-profileAddButton.addEventListener('click', popupAddToggle);
-popupAddCloseButton.addEventListener('click', popupAddToggle);
+profileAddButton.addEventListener('click', handleAddPopup);
+popupAddCloseButton.addEventListener('click', handleAddPopup);
 popupFormsAdd.addEventListener('submit', formAddSubmitHandler);
